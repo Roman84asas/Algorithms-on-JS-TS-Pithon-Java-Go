a8739c7e76f5af2eee0ff2443de00218a12a73f8
RomanMereneanu <asasin84@mail.ru>
2020-10-26 22:35:47 +0200

    Duuble sort on Golang

---
+++ DubbleSort.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func DuubleSort(Args []int) []int {
+	var sortArr = Args
+	var tempVal int
+	for i := 0; i < len(sortArr)-1; i++ {
+		for j := 0; j < len(sortArr)-1; j++ {
+			if sortArr[j] > sortArr[j+1] {
+				tempVal = sortArr[j]
+				sortArr[j] = sortArr[j+1]
+				sortArr[j+1] = tempVal
+			}
+		}
+	}
+	return sortArr
+}
+
+func main() {
+	var arr []int = []int{4, 1, 3, 22, 7}
+	var result []int = DuubleSort(arr)
+	fmt.Println(result)
+}
