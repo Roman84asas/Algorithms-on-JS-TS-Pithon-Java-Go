9f3d67440de021c4eb271ec21f13a71779da54c6
RomanMereneanu <asasin84@mail.ru>
2020-10-29 17:42:35 +0200

    selection sort JS

---
+++ Selection sort/selectionSort.js
@@ -0,0 +1,21 @@
+let arr = [4, 1, 3, 22, 7];
+
+function selectionSort(array) {
+    let tempArr = array;
+    let countArr = tempArr.length - 1;
+    let tempVal = 0;
+
+     for (let i = 0; i < countArr; i++) {
+         let minVal = i;
+        for (let j = 0+1; j < countArr; j++) {
+            if(tempArr[j] < tempArr[minVal]) {
+                minVal = j;
+            }
+        }
+        tempVal = tempArr[i];
+        tempArr[i] = tempArr[minVal];
+        tempArr[minVal] = tempVal;
+    } 
+    return tempArr;
+}
+console.log(selectionSort(arr));
