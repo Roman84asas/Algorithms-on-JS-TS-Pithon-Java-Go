5049832fdd8ecf7317ae6601016736c7593961ec
RomanMereneanu <asasin84@mail.ru>
2020-10-29 17:57:11 +0200

    Selection sort TS

---
+++ Selection sort/SelectionSort.ts
@@ -0,0 +1,30 @@
+const arr: number[] = [4, 1, 3, 22, 7];
+
+class SelectionSort {
+    tempArr: number[];
+    tempValue: number;
+    countArr: number;
+    
+    constructor(sortArr: number[]) {
+        this.tempArr = sortArr;
+        this.countArr = sortArr.length;
+    }
+
+    run() {
+        for (let i = 0; i < this.countArr; i++) {
+            let minVal = i;
+            for (let j = minVal+1; j < this.countArr; j++) {
+                if(this.tempArr[j] < this.tempArr[minVal]) {
+                    minVal = j;
+                }
+            }
+           this.tempValue = this.tempArr[i];
+           this.tempArr[i] = this.tempArr[minVal];
+           this.tempArr[minVal] = this.tempValue;
+       } 
+       return this.tempArr;
+    }
+     
+}
+const selectionSort = new SelectionSort(arr);
+console.log(selectionSort.run());
