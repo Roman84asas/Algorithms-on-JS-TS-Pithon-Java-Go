a90bdd8a348e3ba6fbe7a5bc607d95625b86a2fb
RomanMereneanu <asasin84@mail.ru>
2020-11-08 20:56:22 +0200

    Selection sort python, add class

--- Selection sort/SelectionSort.py
+++ Selection sort/SelectionSort.py
@@ -1,9 +1,16 @@
-def selection_sort(araay):
-    for i, e in enumerate(araay):
-        mn = min(range(i, len(araay)), key=araay.__getitem__)
-        araay[i], araay[mn] = araay[mn], e
-    return araay
+from typing import List
+
+class SelectionSort:
+    def __init__(self, array):
+        self.array = array
+
+    def selection_sort(self, data: List[int]) -> List[int]:
+        for i, e in enumerate(self.array):
+            mn = min(range(i, len(self.array)), key=self.array.__getitem__)
+            self.array[i], self.array[mn] = self.array[mn], e
+        return self.array
+
 
 arr = [4, 1, 3, 22, 7]
-array = selection_sort(arr)
-print(array)
+array = SelectionSort(arr)
+print(array.selection_sort())
